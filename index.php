e96f700a0bde96bada42b6fd542619a6b79f7d1b
Tanay Vardhan <tvardha2@illinois.edu>
2016-11-19 16:58:32 +0000

    11AM

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+    //Do PHP Things like start COOKIES and connect to Database
+
+?>
+
+<!DOCTYPE HTML> 
+
+<html>
+    <head>
+        <title>Sentinel | Home</title>
+        <link rel='stylesheet' href='assets/style.css'>
+    </head>
+    
+    
+    <!-- Static Header -->
+    <div class='header'>
+        <center>SENTINEL</center>
+    </div>
+    
+    
+    <!-- Dynamic Sentinel Status IMG -->
+    <div class='sentinel'>
+    </div>
+    <center class='status'>INACTIVE</center>
+    
+    
+    <!-- Footer Nav -->
+    <div class='navbar'>
+        <center>
+          <ul>
+              <li><div class='icoHolder'></div></li>
+              <li><div class='icoHolder'></div></li>
+              <li><div class='icoHolder'></div></li>
+          </ul>
+        </center>
+    </div>
+    
+</html>
